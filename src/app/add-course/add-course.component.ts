dc6375c550f7e87cc3fed21e6c6eb3d3b74cdd0d
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-06 18:36:20 +0530

    add services and bugfixes

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-course',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AddCourseComponent {
 
+
+  coursetitle=""
+  description=""
+  venue=""
+  duration=""
+  date=""
+
+  constructor(private api:ApiService){
+    
+  }
+
+
+  readValues=()=>{
+    let data:any=
+      {
+        "coursetitle":this.coursetitle,
+        "description":this.description,
+        "venue":this.venue,
+        "duration":this.duration,
+        "date":this.date
+      }
+    
+    console.log(data)
+    this.api.addCourse(data).subscribe(
+      (response)=>{
+        console.log(response)
+      }
+    )
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AddCourseComponent } from './add-course/add-course.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ViewCourseComponent } from './view-course/view-course.component';
 import { Route, RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 const myRoute:Routes=[
   {
@@ -29,7 +32,9 @@ const myRoute:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(myRoute)
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
