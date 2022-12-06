b0af5324570c6b94b5fef7403d8a2b63ea9233d8
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-06 17:23:18 +0530

    add view course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCourseComponent } from './add-course/add-course.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ViewCourseComponent } from './view-course/view-course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddCourseComponent
+    AddCourseComponent,
+    NavbarComponent,
+    ViewCourseComponent
   ],
   imports: [
     BrowserModule,
